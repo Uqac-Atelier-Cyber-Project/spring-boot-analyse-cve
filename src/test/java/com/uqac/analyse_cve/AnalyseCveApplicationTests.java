561377d1f92cf2e4410c6cb2fb269b019e81e147
Aubry TONNERRE <47123188+kiurow590@users.noreply.github.com>
2025-02-07 03:33:38 +0000

    Init commit file

---
+++ src/test/java/com/uqac/analyse_cve/AnalyseCveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqac.analyse_cve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyseCveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
